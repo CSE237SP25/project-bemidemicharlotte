eb1d650026a4676bc87144cf93870fc2563e6c70
CharlotteGuo05 <143556274+CharlotteGuo05@users.noreply.github.com>
2025-03-19 14:50:36 -0500

    Finished fixed deposit class

---
+++ src/tests/FixedDepositTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import bankapp.FixedDeposit;
+
+public class FixedDepositTests {
+		
+	@Test
+	public void testFixedDeposit() {
+		//1. Set up
+		FixedDeposit cdAccount = new FixedDeposit();
+		
+		//2. Call methods
+		cdAccount.processSelection(1, 10000);
+		double finalDeposit=cdAccount.getFinalDeposit();
+		
+		
+		//3. Verify retults
+		assertEquals(finalDeposit, 13097.4, 0.05);
+		
+		
+	}
+	
+
+}

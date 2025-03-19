de020788ffcb17fb00c89d012ae7904eecc4fb03
Emily Han <emily.han@wustl.edu>
2025-03-19 13:40:59 -0500

    Implemented Deposit feature

--- src/bankapp/Menu.java
+++ src/bankapp/Menu.java
@@ -1,5 +1,30 @@
 package bankapp;
+import java.util.Scanner;
 
 public class Menu {
 
+    private BankAccount theAccount;
+
+    public Menu(){
+        theAccount = new BankAccount();
+
+    }
+
+    public void displayOptions(){
+        System.out.println("Please enter deposit amount: ");
+    }
+
+    public double getUserInput(){
+        Scanner keyboardInput = new Scanner(System.in);
+        double userInput = keyboardInput.nextDouble();
+        return userInput;
+    }
+
+    public void processUserInput(double amount){
+        theAccount.deposit(amount);
+    }
+
+    public BankAccount getAccount(){
+        return theAccount;
+    }
 }
